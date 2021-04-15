7a991958cee93d4df67f37de5ead272759963f23
23felons23 <slavikzhyhota@gmail.com>
2021-04-15 14:00:03 +0200

    Add id field to Flink model

--- lib/src/models/flink.dart
+++ lib/src/models/flink.dart
@@ -2,17 +2,19 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Flink {
   //final String title;
+  final String id;
   final String url;
   final String description;
   final Timestamp timestamp;
 
-  Flink({/*this.title,*/ this.url, this.description, this.timestamp});
+  Flink( {this.id,/*this.title,*/ this.url, this.description, this.timestamp});
 
   factory Flink.fromFirestore(DocumentSnapshot doc) {
     Map data = doc.data();
 
     return Flink(
       //title: data['title'] ?? '',
+      id: doc.reference.id,
       url: data['url'] ?? '',
       description: data['description'] ?? '',
       timestamp: data['timestamp'] ?? ''
